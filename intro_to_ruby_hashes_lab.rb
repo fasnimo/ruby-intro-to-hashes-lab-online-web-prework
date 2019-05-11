b4146a0aeddf6337a0580529e90d0524cbd242ec
Rene Cocom <fasnimo@gmail.com>
2019-05-11 03:47:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly
 	  pieces:4, 
 	  names:{reading_railroad:{}, 
 	  pennsylvania_railroad:{}, 
-	  b_and_o_railroad:
+	  b_and_o_railroad:{}, 
 	    
 	  },
 	  rent_in_dollars:{one_piece_owned:25,two_pieces_owned:50,}
