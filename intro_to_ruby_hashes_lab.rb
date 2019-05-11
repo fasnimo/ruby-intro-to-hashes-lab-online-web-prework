a1cdb9a8bf3a184cc6b676fa587d5324b7cab7ea
Rene Cocom <fasnimo@gmail.com>
2019-05-11 03:47:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 def monopoly
 	monopoly = {railroads:{
 	  pieces:4, 
-	  names:{reading_railroad:{}
+	  names:{reading_railroad:{}, pennsylvania_railroad:
 	    
 	  },
 	  rent_in_dollars:{one_piece_owned:25,two_pieces_owned:50,}
